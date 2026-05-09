8d93eb1d95200ff5b84c4ada74727f5b6a0c5ff8
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-05-09 13:44:12 +0100

    add : add base calcauator card and the heading section

---
+++ src/pages/Home.tsx
@@ -0,0 +1,21 @@
+export const Home = () => {
+  return (
+    <main
+      className=" d-flex justify-content-center align-items-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="container card my-auto p-3">
+        <section className="heading-section d-flex justify-content-between align-items-center">
+          <h3>calc</h3>
+          <span>
+            <i className="btn bi bi-1-square-fill fs-3 p-0"></i>
+            <i className="btn bi bi-2-square-fill fs-3 p-0"></i>
+            <i className="btn bi bi-3-square-fill fs-3 p-0"></i>
+          </span>
+        </section>
+        <section></section>
+        <section></section>
+      </div>
+    </main>
+  );
+};
