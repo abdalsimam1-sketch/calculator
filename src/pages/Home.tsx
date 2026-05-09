f87b5d218f03ee4fd6b5c91842dab486ef406cbb
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-05-09 13:50:06 +0100

    add : input and output section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,16 +4,23 @@ export const Home = () => {
       className=" d-flex justify-content-center align-items-center"
       style={{ minHeight: "100vh" }}
     >
-      <div className="container card my-auto p-3">
+      <div className="container card my-auto p-3 d-flex flex-column gap-4">
         <section className="heading-section d-flex justify-content-between align-items-center">
           <h3>calc</h3>
-          <span>
-            <i className="btn bi bi-1-square-fill fs-3 p-0"></i>
-            <i className="btn bi bi-2-square-fill fs-3 p-0"></i>
-            <i className="btn bi bi-3-square-fill fs-3 p-0"></i>
-          </span>
+          <div className="d-flex gap-2 align-items-center">
+            <span className="fw-bold" style={{ fontSize: "10px" }}>
+              THEMES
+            </span>
+            <span>
+              <i className="btn bi bi-1-square-fill fs-3 p-0"></i>
+              <i className="btn bi bi-2-square-fill fs-3 p-0"></i>
+              <i className="btn bi bi-3-square-fill fs-3 p-0"></i>
+            </span>
+          </div>
         </section>
-        <section></section>
+        <section className="input-section card p-3 d-flex">
+          <h1 className="align-self-end">399,983.90</h1>
+        </section>
         <section></section>
       </div>
     </main>
